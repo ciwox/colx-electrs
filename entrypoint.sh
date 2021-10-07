1991f78ed52406df66fc98a80828a8a13c031cbe
Ciwox <ciwox@ciwox.com>
2021-10-07 15:20:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/electrs/target/release/electrs -vvv --timestamp --db-dir /opt/electrs/db --blocks-dir /root/.ColossusXT/blocks --cookie=PuPk6dT4hKVP4tV7:QxWx8GXcNsWK4wFG  --daemon-rpc-addr 127.0.0.1:51473 --network mainnet --monitoring-addr 0.0.0.0:4554 --http-addr 0.0.0.0:3220
+/opt/electrs/target/release/electrs -vvv --timestamp --db-dir /opt/electrs/db --blocks-dir /root/.ColossusXT/blocks --cookie=PuPk6dT4hKVP4tV7:QxWx8GXcNsWK4wFG  --daemon-rpc-addr colossusxt:51473 --network mainnet --monitoring-addr 0.0.0.0:4554 --http-addr 0.0.0.0:3220
